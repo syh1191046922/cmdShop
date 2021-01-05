8734078eea8613f53bbba732cbee2f85f70bc29f
syh1191046922 <1191046922@qq.com>
2021-01-05 17:45:09 +0800

    debug

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,11 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class Test {
+    static Product carts[]=new Product[3];
+    static int count=0;
     public static void main(String args[]) throws ClassNotFoundException {
-        boolean bo = true;
-        while (bo) {
+        boolean bool = true;
+        while (bool) {
             System.out.println("请输入用户名：");
             Scanner sc = new Scanner(System.in);
             String username = sc.next();
@@ -19,77 +21,54 @@ public class Test {
             for (int i = 0; i < users.length; i++) {
                 if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
                     System.out.println("登陆成功");
-                    bo = false;
-
-                    /*显示商品*/
-                    ReadProductExcel readProductExcel = new ReadProductExcel();
-                    Product products[] = readProductExcel.getAllProduct(inPro);
-                    for (Product product : products) {
-                        System.out.print("\t" + product.getpID());
-                        System.out.print("\t" + product.getpName());
-                        System.out.print("\t" + product.getPrice());
-                        System.out.print("\t" + product.getpDesc() + "\n");
-                    }
-
-                    int count = 0;
-                    Product carts[] = new Product[3];//创建购物车（用数组模拟）
-                    System.out.println("请输入商品ID，把该商品加入购物车：");
-                    String pID = sc.next();
-                    ReadProductExcel readProductExcel1 = new ReadProductExcel();
-                    inPro = null;
-                    inPro = Class.forName("Test").getResourceAsStream("/product.xlsx");
-                    Product product = readProductExcel1.getProductById(pID, inPro);
-                    if (product != null) {
-                        carts[count++] = product;
-                    }
-
-                    /*定义方法*/
-
-                    public void shopping() {
-                        readProductExcel = new ReadProductExcel();
-                        inPro = null;
-                        inPro = Class.forName("Test").getResourceAsStream("/product.xlsx");//  /表示的就是classpath
-                        products = readProductExcel.getAllProduct(inPro);
-                        for (Product p : products) {
-                            System.out.print(p.getpID());
-                            System.out.print("\t" + p.getpName());
-                            System.out.print("\t\t" + p.getPrice());
-                            System.out.println("\t\t" + p.getpDesc());
-                        }
-
-                        System.out.println("请输入商品ID，把该商品加入购物车：");
-                        pID = sc.next();
-                        readProductExcel1 = new ReadProductExcel();
-                        inPro = null;
-                        inPro = Class.forName("Test").getResourceAsStream("/product.xlsx");//  /表示的就是classpath
-                        Product product1 = readProductExcel1.getProductById(pID, inPro);
-                        if (product != null) {
-                            carts[count++] = product1;
-                        }
-                    }
-
-
-                    System.out.println("查看购物车输入1：");
-                    System.out.println("继续购物输入2");
-                    int choose = sc.nextInt();
-
-                    if (choose == 1) {
-                        for (int j = 0; j < carts.length; j++) {
-                            if (carts[j] != null) {
-                                System.out.println("\t" + carts[j].getpID());
-                                System.out.println("\t" + carts[j].getpName());
-                                System.out.println("\t" + carts[j].getPrice());
-                                System.out.println("\t" + carts[j].getpDesc());
+                    bool = false;
+                    shopping(sc);
+                    while (true) {
+                        System.out.println("查看购物车输入1：");
+                        System.out.println("继续购物输入2");
+                        System.out.println("结账请按3");
+                        System.out.println("退出请按4");
+                        int choose = sc.nextInt();
+                        if (choose == 1) {
+                            for (Product product : carts) {
+                                if (product != null) {
+                                    System.out.println("\t" + product.getpID());
+                                    System.out.println("\t" + product.getpName());
+                                    System.out.println("\t" + product.getPrice());
+                                    System.out.println("\t" + product.getpDesc());
+                                }
                             }
+                        } else if (choose == 2) {
+                            shopping(sc);
+                        } else if (choose == 4) {
+                            break;
                         }
-                    } else if (choose == 2) {
-                        shopping();
                     }
                     break;
                 } else {
                     System.out.println("登录失败");
                 }
             }
+        }
+    }
+    public static void shopping(Scanner sc)throws ClassNotFoundException {
+        InputStream inPro = Class.forName("Test").getResourceAsStream("/product.xlsx");//  /表示的就是classpath
+        ReadProductExcel readProductExcel = new ReadProductExcel();
+        Product products[] = readProductExcel.getAllProduct(inPro);
+        for (Product product : products) {
+            System.out.print(product.getpID());
+            System.out.print("\t" + product.getpName());
+            System.out.print("\t\t" + product.getPrice());
+            System.out.println("\t\t" + product.getpDesc());
+        }
+        System.out.println("请输入商品ID，把该商品加入购物车：");
+        String pId = sc.next();
+        ReadProductExcel readProductExcel1 = new ReadProductExcel();
+        inPro = null;
+        inPro = Class.forName("Test").getResourceAsStream("/product.xlsx");//  /表示的就是classpath
+        Product product = readProductExcel1.getProductById(pId, inPro);
+        if (product != null) {
+            carts[count++] = product;
         }
     }
 }
